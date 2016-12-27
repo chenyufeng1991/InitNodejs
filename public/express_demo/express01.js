e705abbd72a483e5ec533004e5f7f6f21662d74f
陈宇峰 <yufengcode@gmail.com>
2016-12-27 16:59:47 +0800

    express构建。

---
+++ public/express_demo/express01.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function (request, response) {
+    response.send("Hello World1");
+});
+
+var server = app.listen(8081, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log("应用实例，访问地址为 http://%s:%s", host, port);
+});
