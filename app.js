1f10406c7aae4a2fef27c2ae77bdc7286f866b2a
陈宇峰 <yufengcode@gmail.com>
2016-12-27 16:11:34 +0800

    使用node创建web服务器；根据url来请求任意的html文件。

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
 //加入系统模块
 var http = require('http');
 var os = require('os');
+var url = require('url');
+var fs = require('fs');
 
 //创建服务器，监听3000端口
 http.createServer(function (request, response) {
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.end('1234567890\n');
+    //response.writeHead(200, {'Content-Type': 'text/plain'});
+    //response.end('1234567890\n');
+
+    //以下方法可以使用url来请求任意路径下的每一个html文件
+    var pathname = url.parse(request.url).pathname;
+    console.log("Request for " + pathname + " received");
+    //从文件系统中读取请求的文件内容
+    fs.readFile("./public/html/" + pathname.substr(1), function (err, data) {
+        if(err) {
+            response.writeHead(404, {'Content-Type': 'text/html'});
+        }
+        else {
+            response.writeHead(200, {'Content-Type': 'text/html'});
+            //响应文件内容
+            response.write(data.toString());
+        }
+        // 发送响应数据
+        response.end();
+    });
 }).listen(3000);
 
 console.log('Server running at http://127.0.0.1:3000/');
