80d0245ede0ebc783f623387070c5e1bfa2531a7
陈宇峰 <yufengcode@gmail.com>
2017-01-05 16:31:13 +0800

    加载模块ethereumjs-util，为js中的工具类

--- public/SECP256K1/testSECP256k1.js
+++ public/SECP256K1/testSECP256k1.js
@@ -1,16 +1,22 @@
 var crypto = require('crypto');
 var secp256k1 = require('secp256k1');
+var ethereumjs_util = require('ethereumjs-util');
 
-//需要加密的消息
+/**
+ * 使用椭圆曲线加密算法生成公私钥(secp256k1)
+ *
+ * @type {Buffer}
+ */
+//需要加密的消息，用string初始化buffer
 var msg = new Buffer(32);
 msg.fill("消息");
 
-//生成私钥
+//生成32字节私钥
 var privKey = crypto.randomBytes(32);
 console.log(privKey.toString('hex'));
 
-//生成公钥
-var pubKey = secp256k1.publicKeyCreate(privKey);
+//生成公钥，第二个参数表示是否使用压缩模式
+var pubKey = secp256k1.publicKeyCreate(privKey, false);
 console.log(pubKey.toString('hex'));
 
 // 签名消息
@@ -18,3 +24,6 @@ var sigObj = secp256k1.sign(msg, privKey);
 
 // 验证签名
 console.log(secp256k1.verify(msg, sigObj.signature, pubKey));
+
+console.log("公钥：" + ethereumjs_util.bufferToHex(pubKey));
+console.log("私钥：" + ethereumjs_util.bufferToHex(privKey));
