648844ffa6fcbc06fd174a777aaede13f6d9e04c
陈宇峰 <yufengcode@gmail.com>
2017-01-09 14:46:42 +0800

    接收POST过来的参数

--- public/PostExpress/PostModule01.js
+++ public/PostExpress/PostModule01.js
@@ -1,7 +1,14 @@
 var express = require('express');
 
 module.exports.postModule01 = function (req, res) {
-    console.log("4444444444444444");
-    res.send("44444444444444");
+    //成功接收post过来的数据，并返回json数据，前端只能进行post请求，get则失败
+    console.log("请求的参数：" + req.query.key1 + "       " + req.query.key2);
+
+    var response = {
+        code: 1,
+        message: "请求成功",
+        error: ""
+    };
+    res.send(response);
     res.end();
 };
