2de667eeba70ccc89a3244fab3a7c30e99b97be6
陈宇峰 <yufengcode@gmail.com>
2017-01-09 18:48:04 +0800

    setTimeout设置延迟执行

--- public/1_9/moduleOne.js
+++ public/1_9/moduleOne.js
@@ -4,7 +4,12 @@ module.exports.moduleOne01 = function (req, res, next) {
     console.log("3333333333333333");
     //继续由下一个get去处理
     console.log("请求的参数1：" + req);
-    next();
+
+    //设置延迟执行，异步执行
+    setTimeout(function () {
+       console.log("5555555555555");
+       next();
+    }, 5000);
 };
 
 module.exports.moduleOne02 = function (req, res) {
