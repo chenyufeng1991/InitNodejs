342f0a63adeff8c5d3efe024769310a170e7b4cc
陈宇峰 <yufengcode@gmail.com>
2017-01-09 19:50:45 +0800

    ‘...’挂载路径；装载一组中间件

--- public/testMiddlewareStack/middleStack.js
+++ public/testMiddlewareStack/middleStack.js
@@ -7,7 +7,9 @@ var app = express();
  * 一个get请求里面可以有同步执行的两个路由
  */
 var stackModule = require('./stackModule');
-app.get('/getStack',stackModule.moduleOne01, stackModule.moduleOne02);
+//在一组挂载点装载一组中间件
+//前面的'/getStack' 是挂载路径
+app.get('/getStack',stackModule.moduleOne01, stackModule.moduleOne02, stackModule.moduleOne03);
 
 app.listen(8000, function () {
     console.log("start");
--- public/testMiddlewareStack/stackModule.js
+++ public/testMiddlewareStack/stackModule.js
@@ -1,14 +1,28 @@
 var express = require('express');
 
+/**
+ * 中间件栈
+ *
+ * @param req
+ * @param res
+ * @param next
+ */
 module.exports.moduleOne01 = function (req, res, next) {
     console.log("3333333333333333");
     //继续由下一个get去处理
     console.log("请求的参数1：" + req);
+    //将控制权交给下一个中间件
     next();
 };
 
-module.exports.moduleOne02 = function (req, res) {
+module.exports.moduleOne02 = function (req, res, next) {
     console.log("444444444444444");
     console.log("请求的参数2：" + req);
-    res.send("444444444444444");
+    next();
+};
+
+module.exports.moduleOne03 = function (req, res) {
+    console.log("555555555555");
+    console.log("请求的参数3：" + req);
+    res.send("5555555555");
 };
