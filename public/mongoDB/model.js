7a4181385f065665f0610f35b398b9801f0f8cfb
陈宇峰 <yufengcode@gmail.com>
2017-01-10 22:47:23 +0800

    comment

--- public/mongoDB/model.js
+++ public/mongoDB/model.js
@@ -1,11 +1,14 @@
 var mongoose = require('mongoose');
+//Score为数据库名
 var uri = "mongodb://localhost/Score";
 mongoose.connect(uri);
 
+//可以把Schema理解为表中对象的结构
 var BookSchema = new mongoose.Schema({
     name: String,
     author: String,
     publishTime: Date
 });
 
+//Book可以理解为表名
 mongoose.model('Book', BookSchema);
