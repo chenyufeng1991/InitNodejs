db1a53a021ba9e8f42d6c3432519522fd5c6f406
陈宇峰 <yufengcode@gmail.com>
2017-01-16 20:12:48 +0800

    调整日志输出格式

--- public/log4js/server.js
+++ public/log4js/server.js
@@ -14,23 +14,27 @@ log4js.configure({
             filename: './access.log',
             maxLogSize: 102400,
             backups: 3,
-            category: 'cheese'
+            category: 'normal'
         }
     ],
     replaceConsole: true
 });
 
 //参数要和category中的参数相同
-var logger = log4js.getLogger('cheese');
+var logger = log4js.getLogger('normal');
 //如果设置为INFO，则不会打印出比INFO级别低的日志，如：DEBUG/TRACE.
+//INFO级别及以上的日志会记录在日志文件中
 logger.setLevel('INFO');
 
-app.use(log4js.connectLogger(logger, { level: log4js.levels.INFO}));
+//请求的默认级别是INFO
+//调整日志输入格式
+app.use(log4js.connectLogger(logger, { level: log4js.levels.INFO, format:':method :url'}));
 
 logger.debug("loglog");
 logger.info("infoinfo");
 logger.error("errorerror");
 
+//console.log的方式可以在控制台打印日志，但是无法记录在日志文件中
 console.log("测试");
 console.error("错误");
 
